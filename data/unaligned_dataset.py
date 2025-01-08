0de3a6ce2ca8aef30c387df6ad09f814056e833f
AravindRK99 <aravind1999@gmail.com>
2025-01-07 21:29:22 -0600

    Dataloader with mask for 4 kernels

--- data/unaligned_dataset.py
+++ data/unaligned_dataset.py
@@ -1,18 +1,14 @@
 import os
 from data.base_dataset import BaseDataset
 from data.image_folder import make_dataset
-from PIL import Image
 import random
 import numpy as np 
 import nibabel as nib 
 from scipy.interpolate import interp1d
 import torch
 from torch.utils.data import DataLoader, Dataset
-
-#SPIE data: Used unmasked images 
-#Journal extension: Used masked images for siemens vendors, masked images for C, D. Adding more kernels. 
-
-#To do: Adding more paths for C, D, FC10, FC51 and LUNG kernels. (Maybe even B kernel from Philips). 
+ 
+ #Need to confirm if massk has to be float or int. 
 class UnalignedDataset(BaseDataset):
     """
     Dataset class for mulitpath kernel conversion. Loads in nine kernels and returns the corresponding images.
@@ -31,6 +27,10 @@ class UnalignedDataset(BaseDataset):
         self.dir_B = os.path.join(opt.dataroot, opt.phase + '_siemens_masked_soft')  
         self.dir_C = os.path.join(opt.dataroot, opt.phase + '_ge_bone_hard')
         self.dir_D = os.path.join(opt.dataroot, opt.phase + '_ge_bone_soft')
+        self.mask_dirA = os.path.join(opt.dataroot, opt.phase + '_siemens_masked_hard_slices')
+        self.mask_dirB = os.path.join(opt.dataroot, opt.phase + '_siemens_masked_soft_slices')
+        self.mask_dirC = os.path.join(opt.dataroot, opt.phase + '_ge_bone_hard_slices')
+        self.mask_dirD = os.path.join(opt.dataroot, opt.phase + '_ge_bone_soft_slices')
         print(self.dir_A)
         print(self.dir_B)
         print(self.dir_C)
@@ -39,6 +39,10 @@ class UnalignedDataset(BaseDataset):
         self.B_paths = sorted(make_dataset(self.dir_B, opt.max_dataset_size))    
         self.C_paths = sorted(make_dataset(self.dir_C, opt.max_dataset_size))
         self.D_paths = sorted(make_dataset(self.dir_D, opt.max_dataset_size))
+        self.mask_A_paths = sorted(make_dataset(self.mask_dirA, opt.max_dataset_size))
+        self.mask_B_paths = sorted(make_dataset(self.mask_dirB, opt.max_dataset_size))
+        self.mask_C_paths = sorted(make_dataset(self.mask_dirC, opt.max_dataset_size))
+        self.mask_D_paths = sorted(make_dataset(self.mask_dirD, opt.max_dataset_size))
         self.A_size = len(self.A_paths)  # get the size of dataset A
         self.B_size = len(self.B_paths)  # get the size of dataset B
         self.C_size = len(self.C_paths)
@@ -67,6 +71,7 @@ class UnalignedDataset(BaseDataset):
         """
         # Get the dataitems for 4 domains
         A_path = self.A_paths[index % self.A_size]  # make sure index is within then range
+        A_mask_path = self.mask_A_paths[index % self.A_size]
         if self.opt.serial_batches:   # make sure index is within then range
             index_B = index % self.B_size
             index_C = index % self.C_size
@@ -76,20 +81,23 @@ class UnalignedDataset(BaseDataset):
             index_C = random.randint(0, self.C_size - 1)
             index_D = random.randint(0, self.D_size - 1)
         B_path = self.B_paths[index_B]
+        B_mask_path = self.mask_B_paths[index_B]
         C_path = self.C_paths[index_C]
+        C_mask_path = self.mask_C_paths[index_C]
         D_path = self.D_paths[index_D]
+        D_mask_path = self.mask_D_paths[index_D]
 
         # print("A index:",index % self.A_size)
         # print("B index:",index_B)
         # print("C index:",index_C)
         # print("D index:",index_D)
-        A = self.normalize(A_path)
-        B = self.normalize(B_path)
-        C = self.normalize(C_path)
-        D = self.normalize(D_path) 
+        A, A_mask = self.normalize(A_path, A_mask_path)
+        B, B_mask = self.normalize(B_path, B_mask_path)
+        C, C_mask = self.normalize(C_path, C_mask_path)
+        D, D_mask = self.normalize(D_path, D_mask_path) 
 
         #Return a tuple of the kernel data instead of an indivodual kernel. (Needs to be implemented)
-        return {'A': A, 'B': B, 'C': C, 'D': D,
+        return {'A': A, 'B': B, 'C': C, 'D': D, 'A_mask': A_mask, 'B_mask': B_mask, 'C_mask': C_mask, 'D_mask': D_mask,
                 'A_paths': A_path, 'B_paths': B_path, 'C_paths': C_path, 'D_paths': D_path, } 
 
     def __len__(self):
@@ -102,10 +110,14 @@ class UnalignedDataset(BaseDataset):
         # return max(self.subset_A, self.subset_B, self.subset_C, self.subset_D)
 
 
-    def normalize(self, input_slice_path):
+    def normalize(self, input_slice_path, input_mask_path):
         """Normalize input slice and return as a tensor ranging from [-1,1]"""
         nift_clip = np.clip(nib.load(input_slice_path).get_fdata()[:,:,0], -1024, 3072)
         norm = self.normalizer(nift_clip)
         tensor = torch.from_numpy(norm)
         torch_tensor = tensor.unsqueeze(0).float()
-        return torch_tensor
+        nift_mask = nib.load(input_mask_path).get_fdata()[:,:,0]
+        mask = torch.from_numpy(nift_mask)
+        mask_tensor = mask.unsqueeze(0).float()
+
+        return torch_tensor, mask_tensor
